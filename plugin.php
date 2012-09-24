79a82e0bdc6382c2f343ceae62e01c582d5134e5
Beanow <beanow@oscp.info>
2012-09-24 21:50:33 +0200

    Moved plugin to public repository.

---
+++ plugin.php
@@ -0,0 +1,4 @@
+<?php namespace plugins; if(!defined('TX')) die('No direct access.'); ?>
+
+<link rel="stylesheet" type="text/css" href="<?php echo $plugin; ?>/fullcalendar/fullcalendar.css" />
+<script type="text/javascript" src="<?php echo $plugin; ?>/fullcalendar/fullcalendar.min.js"></script>
